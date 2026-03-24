c63aac0756f3c32228d824c409747ee13f758656
MIGUELANGEL JOSUÉ CRUZ SAGASTUME <sagastumejosue71@gmail.com>
2026-03-24 08:59:37 -0600

    Se agrego un carrusel para darle mayor vista a la pagina

--- parcial_Cruz.js
+++ parcial_Cruz.js
@@ -1,1 +1,6 @@
 //alert("HOLA BIENVENIDO AL PARCIAL")
+const cards = document.querySelectorAll('.card');
+cards.forEach(card => {
+    card.addEventListener('mouseenter', () => card.style.borderColor = '#25D366'); // Color WhatsApp
+    card.addEventListener('mouseleave', () => card.style.borderColor = 'rgba(0,0,0,.125)'); // Color original
+});
