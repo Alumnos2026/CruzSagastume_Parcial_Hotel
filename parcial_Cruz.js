c2d0a30c4047c02d8269057bf8a1ba94e34999de
MIGUELANGEL JOSUÉ CRUZ SAGASTUME <sagastumejosue71@gmail.com>
2026-03-24 09:10:31 -0600

    Se agrego una alert  y un mensaje en el console.log

--- parcial_Cruz.js
+++ parcial_Cruz.js
@@ -1,6 +1,2 @@
-//alert("HOLA BIENVENIDO AL PARCIAL")
-const cards = document.querySelectorAll('.card');
-cards.forEach(card => {
-    card.addEventListener('mouseenter', () => card.style.borderColor = '#25D366'); // Color WhatsApp
-    card.addEventListener('mouseleave', () => card.style.borderColor = 'rgba(0,0,0,.125)'); // Color original
-});
+alert("HOLA BIENVENIDO A LA PAGINA DEL PARCIAL DE INFORMATICA HOTELERA");
+console.log("Bienvenido al parcial");
